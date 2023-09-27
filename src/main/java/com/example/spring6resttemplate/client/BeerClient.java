f7f8030d9035b2111f50c3211ebb5ce2b8d4340a
Olexiy Sokurenko <sokurenko-extern@gvl.de>
2023-09-27 22:48:36 +0200

    Format???

--- src/main/java/com/example/spring6resttemplate/client/BeerClient.java
+++ src/main/java/com/example/spring6resttemplate/client/BeerClient.java
@@ -1,2 +1,22 @@
-package com.example.spring6resttemplate.client;public interface BeerClient {
+package com.example.spring6resttemplate.client;
+
+import com.example.spring6resttemplate.model.BeerDTO;
+import com.example.spring6resttemplate.model.BeerStyle;
+import org.springframework.data.domain.Page;
+
+import java.util.UUID;
+
+public interface BeerClient {
+    Page<BeerDTO> listBeers();
+
+    Page<BeerDTO> listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory, Integer pageNumber,
+                            Integer pageSize);
+
+    BeerDTO getBeerById(UUID beerId);
+
+    BeerDTO createBeer(BeerDTO newDto);
+
+    BeerDTO updateBeer(BeerDTO beerDto);
+
+    void deleteBeer(UUID beerId);
 }
--- src/main/java/com/example/spring6resttemplate/model/BeerStyle.java
+++ src/main/java/com/example/spring6resttemplate/model/BeerStyle.java
@@ -1,2 +1,5 @@
-package com.example.spring6resttemplate.model;public enum BeerStyle {
+package com.example.spring6resttemplate.model;
+
+public enum BeerStyle {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
 }
